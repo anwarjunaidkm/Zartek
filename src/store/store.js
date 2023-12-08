c37f4615c3a808a8b6db9594af0fc9b764bd62e1
ANWAR JUNAID KM <36451800+anwarjunaidkm@users.noreply.github.com>
2023-12-08 14:10:09 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './slice/counterSlice'
 import dataReducer from './slice/dataSlice'
+//
 
 export const store = configureStore({
   reducer: {
